167c9d32ec9fb30aff2757faba733defd1e4d614
unknown <Steffen@Steffen-Laptop.(none)>
2012-06-13 18:45:13 +0200

    Added time measurement

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -186,7 +186,7 @@ void MainWindow::setUpView()
         pa->deleteLater();
 
 //     TODO(steffen) richtige datei öffnen kopieren etc.
-    dataProvider = new DataProvider(QDir::tempPath() + "/" + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmsszzz"));
+    dataProvider = new DataProvider(QDir::tempPath() + "/Octopus-" + QDateTime::currentDateTime().toString("yyyyMMdd_hhmmsszzz"));
     trackScene = new TrackScene(this);
 
     pa = new PresentationArea(trackScene, *dataProvider, ui.hScrollBar, this);
