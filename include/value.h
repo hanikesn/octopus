4e39e6e79a8f2feed68fc23990d09d6a47ce593a
Stefanie Baur <baur.stefanie@gmail.com>
2012-06-19 15:13:57 +0200

    Rückgabewerte Value festgelegt (s. Fkt-Kommentare)

--- include/value.h
+++ include/value.h
@@ -19,7 +19,18 @@ public:
 
     Type getType() const;
 
+    /**
+     * @return The value as a string if the object is of type STRING or DOUBLE.
+     *      An empty string otherwise.
+     *      If the object is of type DOUBLE, the value will be converted to a
+     *      string with the maximum possible precision.
+     */
     QString asString() const;
+
+    /**
+     * @return The value as a double if the object is of type STRING or DOUBLE.
+     *      0.0 otherwise or if the conversion of string to double doesn't work.
+     */
     double asDouble() const;
 
 private:
