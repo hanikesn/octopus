fca5c00cf96fdc4433ed7b78d46a6d0d14576056
domi <domi.olp@googlemail.com>
2012-07-02 15:44:06 +0200

    Funktion in RecordSelection kommentiert

--- include/gui/recordselection.h
+++ include/gui/recordselection.h
@@ -20,6 +20,13 @@ public slots:
 
     void onUpdate();
 
+    /**
+      * Determines in which state the record currently is. In case both timestamps are -1 and
+      * recording is false this object will be set invisible.
+      * @param start Start timestamp of the record
+      * @param end End timestamp of the record or -1 if the record hasn't ended so far.
+      * @param recording State of the recorder (recording/not recording)
+      */
     void onRecord(qint64 start, qint64 end, bool recording);
 
 protected:
