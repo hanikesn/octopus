5c6358ae1cb2373ad6f9f64a09bed84aa039b991
Steffen Hanikel <hanikesn@studi.informatik.uni-stuttgart.de>
2012-07-02 11:19:11 +0200

    Made CSVExporter offset aware

--- src/CSVExporter.cpp
+++ src/CSVExporter.cpp
@@ -1,10 +1,12 @@
 #include "CSVExporter.h"
 #include <QString>
+#include <QStringList>
 #include <QObject>
 #include "dataprovider.h"
 #include "qtextstream.h"
 #include "DBUtil.h"
 #include "value.h"
+#include "abstractdataseries.h"
 
 QString CSVExporter::getFileType()
 {
@@ -15,6 +17,13 @@ void CSVExporter::write(QIODevice &file, const DataProvider &dp, const QStringLi
 {
     QTextStream stream(&file);
 
+    QMap<QString, qint64> offsetMap;
+
+    foreach(QString const& series, dataSeries)
+    {
+        offsetMap.insert(series, dp.getDataSeries(series)->offset);
+    }
+
     Sqlite::PreparedStatement stmt = dp.getDB().getData(dataSeries, begin, end);
 
     QString name;
@@ -24,6 +33,9 @@ void CSVExporter::write(QIODevice &file, const DataProvider &dp, const QStringLi
     for (auto it = stmt.execute(); it != Sqlite::DB::Done; ++it) {
         Sqlite::Row r = *it;
         r >> name >> timestamp >> val;
+        if(offsetMap.contains(name)) {
+            timestamp += offsetMap[name];
+        }
         stream << name << ";" << timestamp << ";" << val.asString() << "\n";
     }
 }
