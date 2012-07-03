f7d590de2b4736c6ebad8230ef7f6b89b8de1a63
domi <domi.olp@googlemail.com>
2012-07-03 15:07:41 +0200

    PresentationArea kommentiert

--- include/gui/presentationarea.h
+++ include/gui/presentationarea.h
@@ -21,6 +21,7 @@ class MouseEventHandler
 {
 public:
     virtual ~MouseEventHandler() {}
+    // Functions to handle mouse input
     virtual void mousePressEvent(QMouseEvent *event) = 0;
     virtual void mouseReleaseEvent(QMouseEvent *event) = 0;
     virtual void mouseMoveEvent(QMouseEvent *event) = 0;
@@ -38,35 +39,83 @@ public:
                               QWidget *parent = 0);
     ~PresentationArea();
 
+    /**
+      * Propagates the save-call to the timeManager and to each of the current tracks.
+      * @param qvm The map in which the necessary data needs to be stored.
+      */
     void save(QVariantMap *qvm);
+
+    /**
+      * Propagates the load-call to the timeManager and to each of the current tracks.
+      * @param qvm The map in which the necessary data is stored.
+      */
     void load(QVariantMap *qvm);
 
     bool hasUnsavedChanges(){return unsavedChanges;}
     void setUnsavedChanges(bool uc);
 
 signals:
+    /**
+      * This signal propagates selections 'onExport(qint64, qint64)' signal.
+      * @param begin Begin of the selection.
+      * @param end End of the selection.
+      */
     void exportRange(qint64 begin, qint64 end);
 
+    /**
+      * This signal is emitted when a ResizeEvent occurs.
+      * @param h The new height.
+      */
     void changedViewHeight(int h);
+
+    /**
+      * This signal is emitted when a ResizeEvent occurs.
+      * @param w The new width.
+      */
     void changedViewWidth(int w);
+
+    /**
+      * This signal is emitted when the margins of the tracks are updated.
+      * @param marginLeft Left margin
+      * @param marginRight Right margin
+      */
     void marginsChanged(int marginLeft, int marginRight);
 
+    /**
+      * This signal propagates selections 'selectionChanged(qint64, qint64)' signal.
+      * @param begin Begin of the selection.
+      * @param end End of the selection.
+      */
     void selectionChanged(qint64 begin, qint64 end);
 
-    void zoomIn(qint64 start, qint64 end);
-
+    /**
+      * This signal propagates the recorders signal 'record(qint64, qint64, bool)' (connect is made
+      * by the ViewManager) to the RecordSelection.
+      * @see RecordSelection for more details.
+      * @param start Start of the selection.
+      * @param end End of the selection.
+      * @param recording True if currently recording, false otherwise.
+      */
     void record(qint64 start, qint64 end, bool recording);
 
 private slots:
+    /**
+      * Shows the SourcesDialog and adds corresponding track(s) by calling 'add(QStringList)'.
+      */
     void onAddTrack();
+
+    /**
+      * Deletes the specified track.
+      * @param t Track to be deleted.
+      */
     void onDelete(Track *t);
 
     void onPlotSettings();
     /**
-      * Is called when the signal rangeChange(qint64, qint64) is emitted.
+      * Is called when the signal rangeChange(qint64, qint64) is emitted by the timeManager.
       * Updates the tracks to the new range.
       * @param begin Begin of the range.
-      * @param end End of the range (usually 30s more than begin)
+      * @param end End of the range
       */
     void onRangeChanged(qint64 begin, qint64 end);
 
@@ -80,6 +129,10 @@ private slots:
     void onNewMax(qint64 max);
 
 protected:
+    /**
+      * Reimplemented from QObject.
+      * Determines whether a Mouse- or ResizeEvent happened.
+      */
     bool eventFilter(QObject *, QEvent *);
 
 private:    
@@ -100,8 +153,14 @@ private:
 
     void addTrack(const QList<QString>& fullDataSeriesNames);
     void addTracks(const QList<QString>& fullDataSeriesNames);
+
+    /**
+      * Adds a track which contains graphs for everey data series in 'fullDataSeriesNames'.
+      * Makes the necessary connects and adds the track widget to the layout.
+      * @param fullDataSeriesNames The names of all data series to be added.
+      * @return A pointer to the created track.
+      */
     Track* add(const QList<QString>& fullDataSeriesNames);
-    int showRecordDialog();
 };
 
 #endif // PRESENTATIONAREA_H
